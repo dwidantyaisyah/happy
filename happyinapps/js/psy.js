79a1815c789677ef72660223a2cedeb9b68602b7
Dasyaaaaaaaaaa1 <151463374+Dasyaaaaaaaaaa1@users.noreply.github.com>
2024-11-16 01:03:04 +0700

    Delete happyinapps directory

--- happyinapps/js/psy.js
+++
@@ -1,9 +0,0 @@
-function greet() {
-    var name = document.getElementById("name").value;
-    alert("Hello, " + name + "! ");
-}
-
-document.getElementById("consultationForm").addEventListener("submit", function(event) {
-    event.preventDefault(); // Prevent form submission for demonstration
-    greet();
-});
